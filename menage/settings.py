45e3ba6b79fd62c365c8e4f68b44d33ea364d960
unknown <dambrebrigitte9>
2022-11-18 14:23:44 +0000

    17

--- menage/settings.py
+++ menage/settings.py
@@ -1,5 +1,6 @@
 import os
 
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -68,10 +69,22 @@ WSGI_APPLICATION = 'menage.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'menage',
+        'USER': 'postgres',
+        'PASSWORD': '12345',
+        'HOST': '127.0.0.1',
+        'PORT': '5432', 
+        #When you install PostgreSQL, the default port number is set to 5432.
     }
 }
 
