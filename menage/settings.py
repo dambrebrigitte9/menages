748b36a03fb3f7a51fac92358badb0645869dce7
unknown <dambrebrigitte9>
2022-11-18 15:41:14 +0000

    20

--- menage/settings.py
+++ menage/settings.py
@@ -69,24 +69,24 @@ WSGI_APPLICATION = 'menage.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'menage',
-        'USER': 'postgres',
-        'PASSWORD': '12345',
-        'HOST': '127.0.0.1',
-        'PORT': '5432', 
-        #When you install PostgreSQL, the default port number is set to 5432.
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'menage',
+#         'USER': 'postgres',
+#         'PASSWORD': '12345',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432', 
+#         #When you install PostgreSQL, the default port number is set to 5432.
+#     }
+# }
 
 
 # Password validation
